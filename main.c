d007e14e41749a380f8498393912cdca1f967d99
NORPG <b10102229@gmail.com>
2019-08-27 23:49:42 +0800

    fix boundary bug  Changes to be committed: 	modified:   main.c

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 			   (Sys_info->uiImageBufBase), 1);;
     for (unsigned char j = 0; j < 255; j++) {
 	sprintf(ch_buf[0], " 0 1 2 3 4 5 6 7 8 9 ");
-	for (int i = 0; (i < strlen(ch_buf[0]) && i < 16); i++) {
+	for (int i = 0; (i < strlen(ch_buf[0]) && i < 24); i++) {
 
 	    dis_num(Sys_info, src, ch_buf[0][i], 0, i * 50);
 	}
